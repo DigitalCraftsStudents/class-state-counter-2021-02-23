9f93ce8c5fef577aeecb43cddf8baa3b7712afd5
Chris Aquino <chris@radishmouse.com>
2021-02-23 15:40:08 -0500

    adds local state for Counter input component

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-function Counter(props) {
-    console.log(props);
-    return (
-        <div>
-            <h1>A Counter!</h1>
-        <p>
-          <button onClick={(e) => props.decrement(3)}>-</button>
-          {props.count}
-          <button onClick={(e) => props.increment(4)}>+</button>
-        </p>
-      </div>
-    )
+
+// inputs, controlled inputs, where should state go?
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props); // gives me a `this.props`
+        // set intital state
+        this.state = {
+            howMuch: 3.14159
+        };
+    }
+    
+    render() {    
+        return (
+            <div>
+                <h1>A Counter !</h1>
+                <p>
+                    <input 
+                        type="number"
+                        value={this.state.howMuch}
+                        onChange={(e) => {
+                            let n = this.state.howMuch;
+                            try {
+                                n = parseFloat(e.target.value);
+                            } catch (e) {
+                                console.log('whoops!')
+                            }
+                            this._setHowMuch(n);
+                        }}
+                    />
+                    <button onClick={(e) => this.props.decrement(this.state.howMuch)}>-</button>
+                    {this.props.count}
+                    <button onClick={(e) => this.props.increment(this.state.howMuch)}>+</button>
+                </p>
+            </div>
+        )
+    }
+
+    _setHowMuch = (howMuch) => {
+        // arrow functions "autobind" `this`
+        this.setState({
+            howMuch
+        }, () => {
+            console.log(`updated howMuch to ${this.state.howMuch}`);
+        });
+    }
 }
 
 export default Counter;
